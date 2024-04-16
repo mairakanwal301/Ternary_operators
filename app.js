68f0d17272ad48fe2d56343a3fde85f9bf2ad0c8
mairarafiq301 <mairarafiq301@gmail.com>
2024-04-16 12:02:55 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+var userName = "Mairakanwal";
+/*if (userName == "Mairakanwal") {
+    console.log("you are successfully login");
+} else {
+    console.log("invalid name");
+    
+}*/
+userName == "Mairakanwal"
+    ? console.log("you are succesfully login") : console.log("invalid name");
---
+++ app.ts
@@ -0,0 +1,12 @@
+let userName = "Mairakanwal" ;
+
+/*if (userName == "Mairakanwal") {
+    console.log("you are successfully login");
+} else {
+    console.log("invalid name");
+    
+}*/
+
+ userName == "Mairakanwal"
+?console.log("you are succesfully login") : console.log("invalid name")
+
